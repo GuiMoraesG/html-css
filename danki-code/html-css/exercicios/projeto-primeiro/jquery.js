dd15981f01d4157aaa110707c532e501b4300a96
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-06-01 13:49:51 -0300

    abrir menu com JQuery

---
+++ danki-code/html-css/exercicios/projeto-primeiro/jquery.js
@@ -0,0 +1,3 @@
+$('#menu').click(() => {
+    $('#navegador').slideToggle()
+})
---
+++ danki-code/html-css/exercicios/projeto-primeiro/main.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    if (navegador.style.display == 'block') {
+        navegador.style.display = 'none'
+    } else {
+        navegador.style.display = 'block'
+    }
+}

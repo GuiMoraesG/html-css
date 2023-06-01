34249df4bae6c413708be2e7ea26c51cfc76c9a3
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-06-01 14:07:59 -0300

    Scroll dinâmico

--- danki-code/html-css/exercicios/projeto-primeiro/jquery.js
+++ danki-code/html-css/exercicios/projeto-primeiro/jquery.js
@@ -1,3 +1,10 @@
 $('#menu').click(() => {
     $('#navegador').slideToggle()
 })
+
+$('nav a').click(() => {
+    let href = $(this).attr('href')
+    let offSetTop = $(href).offset().top
+
+    $('html, body').animate({ 'scrollTop': offSetTop })
+})
